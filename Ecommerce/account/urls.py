f3fdd114cc5352f424098e91da9184eac0361db8
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-04-17 18:01:03 +0430

    Correction

--- Ecommerce/account/urls.py
+++ Ecommerce/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
+
 app_name = 'account'
 urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+
 ]
