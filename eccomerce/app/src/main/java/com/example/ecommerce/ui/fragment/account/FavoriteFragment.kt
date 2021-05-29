46e90404e55f3ed7cec419a3ad1671476c363496
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-29 21:09:06 +0430

    create FavoriteFragment.kt and fragment_favorite.xml update ProfileFragment.kt

--- eccomerce/app/src/main/java/com/example/ecommerce/ui/fragment/account/FavoriteFragment.kt
+++ eccomerce/app/src/main/java/com/example/ecommerce/ui/fragment/account/FavoriteFragment.kt
@@ -6,13 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.ecommerce.R
 import com.example.ecommerce.utils.Fragment
-import com.example.ecommerce.viewmodel.LoginViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class ProfileFragment : Fragment() {
-
-    private val loginViewModel:LoginViewModel by viewModel()
+class FavoriteFragment : Fragment() {
 
 
     override fun onCreateView(
@@ -20,18 +16,12 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loginViewModel.checkLogin()
-        loginViewModel.checkLoginStatus.observe(viewLifecycleOwner){
-
-        }
-        loginViewModel.progressbarLiveData.observe(viewLifecycleOwner) {
-            progress(it)
-        }
     }
+
 
 }
