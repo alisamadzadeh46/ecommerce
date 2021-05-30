bd2f5018fccf292320d6f8d040c9f86d0f3f692e
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-30 14:19:57 +0430

    complete CategoryFragment.kt and fragment_category.xml

--- eccomerce/app/src/main/java/com/example/ecommerce/ui/fragment/category/CategoryFragment.kt
+++ eccomerce/app/src/main/java/com/example/ecommerce/ui/fragment/category/CategoryFragment.kt
@@ -1,34 +1,23 @@
-package com.example.ecommerce
+package com.example.ecommerce.ui.fragment.category
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ecommerce.R
+import com.example.ecommerce.ui.adapter.CategoryAdapter
+import com.example.ecommerce.utils.Fragment
+import com.example.ecommerce.viewmodel.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_category.*
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val homeViewModel: HomeViewModel by viewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,23 +26,15 @@ class CategoryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel.categoryLiveDate.observe(viewLifecycleOwner) {
+            val categoryAdapter: CategoryAdapter by inject { parametersOf(it) }
+            category.adapter = categoryAdapter
+        }
+        category.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     }
+
 }
